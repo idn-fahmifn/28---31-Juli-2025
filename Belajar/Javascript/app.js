da8888ec93c10c7e9e3eae1c5f1dc0db66b91274
idn-fahmifn <fahmifn.idn@gmail.com>
2025-07-30 10:38:05 +0700

    3.1

---
+++ Belajar/Javascript/app.js
@@ -0,0 +1,19 @@
+
+// tipe data
+// tipe data string digunakan untuk text
+let mobil = "toyota" //string.
+let mobilSaya = `Saya punya mobil. Mereknya adalah ${mobil}`; //string dengan template literal
+console.log(mobilSaya) //output
+
+
+//================================= 
+// const tetap tidak bisa diubah.
+const gender = "laki laki";
+
+// let bisa di reusable / diubah.
+let nama = "Fahmi"; //nilai pertama
+nama = "Ilham"; //perubahan pertama
+nama = "Fadhil"; // perubaha terakhir
+
+console.log(nama); //output : Fadhil
+console.log(gender); //output : laki laki
